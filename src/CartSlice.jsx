fc460b5854600d32838ac0f5f4e1dddd3cdf641f
Mabudu Jabateh <wiseobinnamj33@gmai.com>
2024-12-09 15:46:33 +0000

    part3

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -22,17 +22,18 @@ export const CartSlice = createSlice({
 
     
     },
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
 
   },
-  addItem: (state, action) => {
-    const { name, image, cost } = action.payload;
-    const existingItem = state.items.find(item => item.name === name);
-    if (existingItem) {
-      existingItem.quantity++;
-    } else {
-      state.items.push({ name, image, cost, quantity: 1 });
-    }
-  },
+ 
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
